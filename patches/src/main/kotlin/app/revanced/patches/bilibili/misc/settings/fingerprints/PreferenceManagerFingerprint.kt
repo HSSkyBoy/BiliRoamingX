8fe453f7e0bec05f37dca87114b44b0d27312a8f
Neko Beillc <nikobe918@outlook.com>
2026-08-29 14:30:38 +0800

    fix(settings): 放寬 PreferenceManagerFingerprint 指紋參數以相容 9.8.0 混淆的 androidx.preference.g

--- patches/src/main/kotlin/app/revanced/patches/bilibili/misc/settings/fingerprints/PreferenceManagerFingerprint.kt
+++ patches/src/main/kotlin/app/revanced/patches/bilibili/misc/settings/fingerprints/PreferenceManagerFingerprint.kt
@@ -3,7 +3,5 @@ package app.revanced.patches.bilibili.misc.settings.fingerprints
 import app.revanced.patcher.fingerprint.MethodFingerprint
 
 object PreferenceManagerFingerprint : MethodFingerprint(
-    strings = listOf("_has_set_default_values"),
-    parameters = listOf("Landroid/content/Context;", "Ljava/lang/String;", "I", "I", "Z"),
-    returnType = "V"
+    strings = listOf("_has_set_default_values")
 )
