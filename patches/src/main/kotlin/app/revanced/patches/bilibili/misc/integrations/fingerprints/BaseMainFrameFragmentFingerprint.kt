e5052b91b30b14e5107587f2fc9c1f5adfba62a1
Neko Beillc <nikobe918@outlook.com>
2026-08-29 14:05:18 +0800

    fix(fingerprint): 放寬 BaseMainFrameFragment 指紋參數限制以適配 9.8.0

--- patches/src/main/kotlin/app/revanced/patches/bilibili/misc/integrations/fingerprints/BaseMainFrameFragmentFingerprint.kt
+++ patches/src/main/kotlin/app/revanced/patches/bilibili/misc/integrations/fingerprints/BaseMainFrameFragmentFingerprint.kt
@@ -3,7 +3,5 @@ package app.revanced.patches.bilibili.misc.integrations.fingerprints
 import app.revanced.patcher.fingerprint.MethodFingerprint
 
 object BaseMainFrameFragmentFingerprint : MethodFingerprint(
-    strings = listOf("home.menu_update_enable"),
-    parameters = listOf("Ljava/util/List;"),
-    returnType = "V"
+    strings = listOf("home.menu_update_enable")
 )
