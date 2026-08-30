9958fc9f3ac4563d1ad1fac7ddd68439c35be33e
Neko Beillc <nikobe918@outlook.com>
2026-08-30 23:08:09 +0800

    fix(theme): 9.8.0 自定義主題色補丁安全跳過缺失特徵

--- patches/src/main/kotlin/app/revanced/patches/bilibili/misc/theme/patch/CustomThemePatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/bilibili/misc/theme/patch/CustomThemePatch.kt
@@ -34,135 +34,143 @@ object CustomThemePatch : BytecodePatch(
     )
 ) {
     override fun execute(context: BytecodeContext) {
-        val patchClass = context.findClass("Lapp/revanced/bilibili/patches/CustomThemePatch;")!!.mutableClass
+        val patchClass = context.findClass("Lapp/revanced/bilibili/patches/CustomThemePatch;")?.mutableClass ?: return
 
-        ThemeNameFingerprint.result?.mutableClass?.fields?.first {
+        ThemeNameFingerprint.result?.mutableClass?.fields?.firstOrNull {
             it.type == "Ljava/util/Map;"
         }?.let { field ->
             field.accessFlags = field.accessFlags.toPublic().removeFinal()
             patchClass.methods.run {
-                first { it.name == "getThemeNames" }.also { remove(it) }.cloneMutable(
+                firstOrNull { it.name == "getThemeNames" }?.also { remove(it) }?.cloneMutable(
                     registerCount = 1, clearImplementation = true
-                ).apply {
+                )?.apply {
                     addInstructions(
                         """
                         sget-object v0, $field
                         return-object v0
                     """.trimIndent()
                     )
-                }.also { add(it) }
+                }?.also { add(it) }
             }
-        } ?: throw ThemeNameFingerprint.exception
+        }
 
         ThemeHelperFingerprint.result?.let { r ->
-            r.mutableClass.fields.first { it.name == r.method.name }
+            r.mutableClass.fields.firstOrNull { it.name == r.method.name }
         }?.let { field ->
             field.accessFlags = field.accessFlags.toPublic().removeFinal()
             patchClass.methods.run {
-                first { it.name == "getColorArray" }.also { remove(it) }.cloneMutable(
+                firstOrNull { it.name == "getColorArray" }?.also { remove(it) }?.cloneMutable(
                     registerCount = 1, clearImplementation = true
-                ).apply {
+                )?.apply {
                     addInstructions(
                         """
                         sget-object v0, $field
                         return-object v0
                     """.trimIndent()
                     )
-                }.also { add(it) }
+                }?.also { add(it) }
             }
-        } ?: throw ThemeHelperFingerprint.exception
+        }
 
-        (BuiltInThemesFingerprint.result ?: BuiltInThemesFingerprint2.result)?.mutableClass?.fields?.first {
+        (BuiltInThemesFingerprint.result ?: BuiltInThemesFingerprint2.result)?.mutableClass?.fields?.firstOrNull {
             it.type == "Ljava/util/Map;"
         }?.let { field ->
             field.accessFlags = field.accessFlags.toPublic().removeFinal()
             patchClass.methods.run {
-                first { it.name == "getAllThemes" }.also { remove(it) }.cloneMutable(
+                firstOrNull { it.name == "getAllThemes" }?.also { remove(it) }?.cloneMutable(
                     registerCount = 1, clearImplementation = true
-                ).apply {
+                )?.apply {
                     addInstructions(
                         """
                         sget-object v0, $field
                         return-object v0
                     """.trimIndent()
                     )
-                }.also { add(it) }
+                }?.also { add(it) }
             }
-        } ?: throw BuiltInThemesFingerprint.exception
+        }
 
-        ThemeColorsFingerprint.result?.mutableClass?.methods?.first {
+        ThemeColorsFingerprint.result?.mutableClass?.methods?.firstOrNull {
             it.name == "<init>" && it.accessFlags.isPrivate()
         }?.let { method ->
             method.accessFlags = method.accessFlags.toPublic()
-            val garbType = method.parameterTypes[0].toString()
-            garbType.toClassDef(context).proxy(context).addDefaultConstructorIfNeeded()
-            patchClass.methods.run {
-                first { it.name == "newTheme" }.also { remove(it) }.cloneMutable(
-                    registerCount = 35, clearImplementation = true
-                ).apply {
-                    addInstructions(
-                        """
-                        new-instance v0, ${method.definingClass}
-                        move-object v1, v0
-                        new-instance v2, $garbType
-                        invoke-direct {v2}, $garbType-><init>()V
-                        move-object/from16 v3, p0
-                        move-wide/from16 v4, p1
-                        move-wide/from16 v6, p3
-                        move-wide/from16 v8, p5
-                        move-wide/from16 v10, p7
-                        move-wide/from16 v12, p9
-                        move-wide/from16 v14, p11
-                        move-wide/from16 v16, p13
-                        move/from16 v18, p15
-                        invoke-direct/range {v1 .. v18}, $method
-                        return-object v0
-                    """.trimIndent()
-                    )
-                }.also { add(it) }
+            val garbType = method.parameterTypes.getOrNull(0)?.toString()
+            if (garbType != null) {
+                garbType.toClassDef(context).proxy(context).addDefaultConstructorIfNeeded()
+                patchClass.methods.run {
+                    firstOrNull { it.name == "newTheme" }?.also { remove(it) }?.cloneMutable(
+                        registerCount = 35, clearImplementation = true
+                    )?.apply {
+                        addInstructions(
+                            """
+                            new-instance v0, ${method.definingClass}
+                            move-object v1, v0
+                            new-instance v2, $garbType
+                            invoke-direct {v2}, $garbType-><init>()V
+                            move-object/from16 v3, p0
+                            move-wide/from16 v4, p1
+                            move-wide/from16 v6, p3
+                            move-wide/from16 v8, p5
+                            move-wide/from16 v10, p7
+                            move-wide/from16 v12, p9
+                            move-wide/from16 v14, p11
+                            move-wide/from16 v16, p13
+                            move/from16 v18, p15
+                            invoke-direct/range {v1 .. v18}, $method
+                            return-object v0
+                        """.trimIndent()
+                        )
+                    }?.also { add(it) }
+                }
             }
-        } ?: throw ThemeColorsFingerprint.exception
+        }
 
-        val onSetSkinListMethod = patchClass.methods.first { it.name == "onSetSkinList" }
-        SkinListFingerprint.result?.mutableMethod?.addInstructions(
-            0, """
-            invoke-static {p1}, $onSetSkinListMethod
-        """.trimIndent()
-        ) ?: throw SkinListFingerprint.exception
+        val onSetSkinListMethod = patchClass.methods.firstOrNull { it.name == "onSetSkinList" }
+        if (onSetSkinListMethod != null) {
+            SkinListFingerprint.result?.mutableMethod?.addInstructions(
+                0, """
+                invoke-static {p1}, $onSetSkinListMethod
+            """.trimIndent()
+            )
+        }
 
         val onClickOriginListenerType = "Lapp/revanced/bilibili/widget/OnClickOriginListener;"
-        val onThemeClickMethod = patchClass.methods.first { it.name == "onThemeClick" }
-        ThemeClickFingerprint.result?.run {
-            mutableClass.interfaces.add(onClickOriginListenerType)
-            mutableMethod.also {
-                mutableClass.methods.add(it.cloneMutable(name = "onClick_Origin"))
-            }.addInstructionsWithLabels(
-                0, """
-                invoke-static {p0, p1}, $onThemeClickMethod
-                move-result v0
-                if-eqz v0, :jump
-                return-void
-                :jump
-                nop
-            """.trimIndent()
-            )
-        } ?: throw ThemeClickFingerprint.exception
+        val onThemeClickMethod = patchClass.methods.firstOrNull { it.name == "onThemeClick" }
+        if (onThemeClickMethod != null) {
+            ThemeClickFingerprint.result?.run {
+                mutableClass.interfaces.add(onClickOriginListenerType)
+                mutableMethod.also {
+                    mutableClass.methods.add(it.cloneMutable(name = "onClick_Origin"))
+                }.addInstructionsWithLabels(
+                    0, """
+                    invoke-static {p0, p1}, $onThemeClickMethod
+                    move-result v0
+                    if-eqz v0, :jump
+                    return-void
+                    :jump
+                    nop
+                """.trimIndent()
+                )
+            }
+        }
 
-        val onThemeResetMethod = patchClass.methods.first { it.name == "onThemeReset" }
-        ThemeProcessorFingerprint.result?.mutableClass?.methods?.filter { m ->
-            m.parameterTypes.isEmpty() && m.accessFlags == 0 && m.returnType == "V"
-        }?.forEach { m ->
-            m.addInstructionsWithLabels(
-                0, """
-                invoke-static {}, $onThemeResetMethod
-                move-result v0
-                if-eqz v0, :jump
-                return-void
-                :jump
-                nop
-            """.trimIndent()
-            )
-        } ?: throw ThemeProcessorFingerprint.exception
+        val onThemeResetMethod = patchClass.methods.firstOrNull { it.name == "onThemeReset" }
+        if (onThemeResetMethod != null) {
+            ThemeProcessorFingerprint.result?.mutableClass?.methods?.filter { m ->
+                m.parameterTypes.isEmpty() && m.accessFlags == 0 && m.returnType == "V"
+            }?.forEach { m ->
+                m.addInstructionsWithLabels(
+                    0, """
+                    invoke-static {}, $onThemeResetMethod
+                    move-result v0
+                    if-eqz v0, :jump
+                    return-void
+                    :jump
+                    nop
+                """.trimIndent()
+                )
+            }
+        }
 
         WebActivityBuildUriFingerprint.result?.method?.implementation?.instructions?.firstNotNullOfOrNull { inst ->
             if (inst.opcode == Opcode.INVOKE_STATIC) inst.getReference<MethodReference>().let {
@@ -173,17 +181,17 @@ object CustomThemePatch : BytecodePatch(
         }?.let { field ->
             field.accessFlags = field.accessFlags.toPublic().removeFinal()
             patchClass.methods.run {
-                first { it.name == "getColorIds" }.also { remove(it) }.cloneMutable(
+                firstOrNull { it.name == "getColorIds" }?.also { remove(it) }?.cloneMutable(
                     registerCount = 1, clearImplementation = true
-                ).apply {
+                )?.apply {
                     addInstructions(
                         """
                         sget-object v0, $field
                         return-object v0
                     """.trimIndent()
                     )
-                }.also { add(it) }
+                }?.also { add(it) }
             }
-        } ?: throw WebActivityBuildUriFingerprint.exception
+        }
     }
 }
